86d0e58781ff6d135c7d25af32c171b5e47486b2
erickadikah <erickadikah2030@gmail.com>
2022-12-07 01:04:27 +0300

    new

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,11 +9,61 @@ from models.review import Review
 from models.base_model import Base
 import mySQLdb
 from os import getenv
-user = getenv("HBNB_MYSQL_USER")
-passwd = getenv("HBNB_MYSQL_PWD")
-host = getenv("localhost")
-db = getenv("HBNB_MYSQL_DB")
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 
-self.__engine = engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(user, passwd, host, db, (pool_pre_ping=True))
-        if getenv(HBNB_ENV) == "test":
+
+class DBStorage():
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv('HBNB_MYSQL_USER')
+        passwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+
+        self.__engine = engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(user, passwd, host, db), pool_pre_ping=True)
+
+        if env == "test":
         Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+    if cls = is None:
+         objs = self.__session.query(User).all()
+         objs.extend(self.__session.query(State).all())
+         objs.extend(self.__session.query(City).all())
+         objs.extent(self.__session.query(Amenity).all())
+         objs.extend(self.__session.query(Place).all())
+         objs.extend(self.__session.query(Review).all())
+
+    else:
+         if type(cls) == str:
+            cls = eval(cls)
+            obj = self.__session.query(cls)
+
+        return {"{}.{}".format(type(obj).__name__, obj.id) obj for obj in objs}
+
+    def new(self, obj):
+    self.session.add(obj)
+    """a funtion to add current database session"""
+
+    def save(self):
+    self.session.commit()
+    """a function to save"""
+
+    def delete(self, obj=none):
+    if obj in not None:
+        self.session.delete(obj)
+    """a function to delete current database session"""
+
+    def reload(self):
+    Base.metadata.create_all(self.__engine)
+    session.factory = session.maker(bind=self.__engine,
+                                    expire_on_commit=False)
+    session = scoped_session(session_factory)
+    self.__session = Session()
+
+    def close(self):
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -3,10 +3,11 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.orm import relationship
 
 class State(BaseModel):
     """ State class """
     __tablename__ = "state"
     name = Column(String(128), nullable=False)
-    cities = relationship('City')
+    cities = relationship('City' cascade='all , delete, delete=orphan',
+                            back_populates='state')
