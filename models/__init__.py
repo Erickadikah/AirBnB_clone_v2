81473200c6cf6989875166e0d46a793978d0a91d
erickadikah <erickadikah2030@gmail.com>
2022-12-06 19:33:43 +0300

    new

--- models/__init__.py
+++ models/__init__.py
@@ -6,18 +6,10 @@ from os import getenv
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
-    from models.engine.bd_storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
-
-
-
-"""
-storage = FileStorage()
-storage.reload()
-"""
