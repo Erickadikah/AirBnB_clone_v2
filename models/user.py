3fa6c873d6dc0e903219678e941f923b2cb99ec1
Adewole <phatboislym@protonmail.com>
2022-12-08 02:02:31 +0100

    fix: update user module (4)

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module for user class, inherits from the Base and BaseModel classes"""
-from models.base_mode import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 
 
