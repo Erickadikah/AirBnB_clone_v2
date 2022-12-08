f6b4188a271f3139aa6dc326e53f80b0d0c737ea
Adewole <phatboislym@protonmail.com>
2022-12-08 01:43:53 +0100

    fix: update user module (2)

--- models/user.py
+++ models/user.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """module for user class, inherits from the Base and BaseModel classes"""
-from models.base_model import BaseModel
-from models.base_model import Base
+from base_model import BaseModel
+from base_model import Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
