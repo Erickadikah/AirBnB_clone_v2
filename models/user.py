9efc284fd9d3751a4b9bd9fa80c4614d2ba59014
Adewole <phatboislym@protonmail.com>
2022-12-08 01:32:37 +0100

    fix: update user module

--- models/user.py
+++ models/user.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Represents a user for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table users.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-"""
+"""module for user class, inherits from the Base and BaseModel classes"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-    places = ''
-    reviews = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
