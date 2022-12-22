52692595f83e28ebf4252214f17bd6f55e13665e
erickadikah <erickadikah2030@gmail.com>
2022-12-22 13:56:14 +0300

    new

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,48 +1,37 @@
 #!/usr/bin/env bash
-# sets up my web servers for the deployment of web_static
+#A Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install nginx -y
 
-echo -e "\e[1;32m START\e[0m"
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
 
-#--Updating the packages
-sudo apt-get -y update
-sudo apt-get -y install nginx
-echo -e "\e[1;32m Packages updated\e[0m"
-echo
+#adding a test string
 
-#--configure firewall
-sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
-echo
+echo "Welcome to my server" | sudo tee /data/web_static/releases/test/index.html
 
-#--created the dir
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "\e[1;32m directories created"
-echo
+echo "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-#--adds test string
-echo "<h1>Welcome to www.beta-scribbles.tech</h1>" > /data/web_static/releases/test/index.html
-echo -e "\e[1;32m Test string added\e[0m"
-echo
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
 
-#--prevent overwrite
-if [ -d "/data/web_static/current" ];
-then
-    echo "path /data/web_static/current exists"
-    sudo rm -rf /data/web_static/current;
-fi;
-echo -e "\e[1;32m prevent overwrite\e[0m"
-echo
+        error_page 404 /404.html;
+        location = /404.html {
+                internal;
+        }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        location /hbnb_static {
+                alias /data/web_static/current/;
+        }
 
-#--create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+}" | sudo tee /etc/nginx/sites-available/default
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+sudo rm -rf /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
 sudo chown -hR ubuntu:ubuntu /data
-
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-echo -e "\e[1;32m Symbolic link created\e[0m"
-echo
-
-#--restart NGINX
 sudo service nginx restart
-echo -e "\e[1;32m restart NGINX\e[0m"
