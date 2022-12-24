4a2478ad70c545a0b1005e80ffa712f9cb00e7ae
erickadikah <erickadikah2030@gmail.com>
2022-12-24 15:25:32 +0300

    fabfile

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from
+the contents of the web_static folder"""
+from fabric.api import local
+from datatime import datetime
+
+
+def do_pack():
+     """generating a .tgz archive from the contents of the web_static"""
+    local('mkdir -p versions')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(time)
+    file_name = "versions/{}".format(name)
+    location = local("tar -cvzf {} web_static/".format(file_name))
+    if location:
+        return (location)
+    else:
+        return None
--- fabfile.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from fabric.api import *
-
-def deploy():
-    local('web_static')
