2952eeeffa7ae5041a60ed4db074a06149518138
erickadikah <erickadikah2030@gmail.com>
2023-01-13 23:00:29 +0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,31 +1,49 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
+"""
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello_route():
+    """prints hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb/', strict_slashes=False)
 def hbnb():
+    """prints HBNB"""
     return 'HBNB'
-"""
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def laugh(text):
+def c_text(text):
+    """"prints C followed by <text> content"""
     text = text.replace('_', " ")
     return 'C {}'.format(text)
-"""
+
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python(text="is cool"):
+    """prints python is cool"""
     text = text.replace('_', " ")
     return "Python %s" % text
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_int(n):
-    """checks if argument passed is and interger
-    """
-    if type(n) is int:
+    """checks if argument passed is and interger"""
+    if isinstance(n, int):
         return "{} is an interger".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
