427ba9f9cbbe8d7f6d386815c4b6035b2602337a
erickadikah <erickadikah2030@gmail.com>
2023-01-18 10:16:18 +0300

    task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def get_list():
+def state_list():
     """Get list of states from db"""
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
